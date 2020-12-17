31283529c03a06ccc63dc67d314b97c57d4f788f
Anna Tereshko <anna.26.tereshko@gmail.com>
2020-12-17 15:55:41 +0300

    feat: fix output chart(months -> days)

--- src/chart.js
+++ src/chart.js
@@ -1,65 +1,60 @@
 const Chart = require('chart.js');
 
-const getMonthKey = (date) => `${date.getFullYear()}_${date.getMonth()}`;
+const DATASETS = [
+  { key: 'cases', label: 'Active cases', borderColor: 'rgb(255, 99, 132)' },
+  { key: 'deaths', label: 'Deaths cases', borderColor: 'rgb(55, 99, 132)' },
+  { key: 'recovered', label: 'Recovered cases', borderColor: 'rgb(25, 99, 132)' },
+];
+
 export default class Charts {
   constructor() {
-    this.months = [];
-    this.dataSets = [];
+    this.days = [];
+    this.chartData = [];
   }
 
   setData(data) {
-    this.initMonths();
-    const grouppedData = data.flat().reduce((result, item) => {
-      const date = new Date(item.date);
-      const monthKey = getMonthKey(date);
+    this.days = data.map((day) => new Date(day.date));
 
-      const monthData =
-        result[monthKey] ||
-        (result[monthKey] = {
-          cases: 0,
-          deaths: 0,
-          recovered: 0,
-        });
-      monthData.cases += item.cases;
-      monthData.deaths += item.deaths;
-      monthData.recovered += item.recovered;
-
-      return result;
-    }, {});
-
-    this.dataSets = [
-      { key: 'cases', label: 'Active cases', borderColor: 'rgb(255, 99, 132)' },
-      { key: 'deaths', label: 'Deaths cases', borderColor: 'rgb(55, 99, 132)' },
-      { key: 'recovered', label: 'Recovered cases', borderColor: 'rgb(25, 99, 132)' },
-    ].map((el) => ({
+    this.chartData = DATASETS.map((el) => ({
       ...el,
-      data: this.months.map((month) => (grouppedData[getMonthKey(month)] || {})[el.key] || 0),
+      data: data.map((item) => item[el.key] || 0),
     }));
 
     this.render();
   }
 
-  initMonths() {
-    this.months = [];
-    let date = new Date('2020-03-01T00:00:00Z');
-
-    while (date <= new Date()) {
-      this.months.push(date);
-
-      date = new Date(date.getTime());
-      date.setMonth(date.getMonth() + 1);
-    }
-  }
-
   render() {
-    const MULTILINEONEFILL = global.document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(MULTILINEONEFILL, {
+    const CHART = global.document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(CHART, {
       type: 'line',
       data: {
-        labels: this.months.map((x) => x.toLocaleString('default', { month: 'short' })),
-        datasets: this.dataSets,
+        labels: this.days,
+        datasets: this.chartData,
       },
-      options: {},
+      options: {
+        elements: {
+          point: {
+            radius: 0,
+          },
+        },
+        animation: {
+          easing: 'easeOutCirc',
+          duration: 1000,
+        },
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              position: 'bottom',
+              time: {
+                displayFormats: { month: 'MMM' },
+                tooltipFormat: 'ddd D',
+                unit: 'month',
+              },
+            },
+          ],
+        },
+      },
     });
   }
 }
