c0d753952bc993b8fe7b1945e396dcb9e674b4e8
Anna Tereshko <anna.26.tereshko@gmail.com>
2020-12-18 19:23:40 +0300

    feat: add function setupResizeButtons

--- src/index.js
+++ src/index.js
@@ -9,4 +9,28 @@ function update() {
   });
 }
 
+function setupResizeButtons() {
+  const boxes = global.document.querySelectorAll('.content-box');
+
+  function toggleButton() {
+    this.parentElement.classList.toggle('expanded');
+  }
+
+  boxes.forEach((el) =>
+    el.addEventListener('mousemove', function () {
+      const span = this.querySelector('span');
+      span.classList.add('expand');
+      span.addEventListener('click', toggleButton);
+    })
+  );
+  boxes.forEach((el) =>
+    el.addEventListener('mouseleave', function () {
+      const span = this.querySelector('span');
+      span.classList.remove('expand');
+      span.removeEventListener('click', toggleButton);
+    })
+  );
+}
 update();
+
+setupResizeButtons();
